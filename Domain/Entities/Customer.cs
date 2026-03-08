1f99658a10beadc5a9c55bf5e6eb32f6e5d53b10
Jason Govender <jasongovend@gmail.com>
2026-03-08 09:40:52 +0200

    Created Customer Entity

--- Domain/Entities/Customer.cs
+++ Domain/Entities/Customer.cs
@@ -1,5 +1,8 @@
+using SwiftCart.Domain.Enums;
+
 namespace SwiftCart.Domain.Entities;
 
-public class Customer
+public class Customer : User
 {
+    public override UserRole Role => UserRole.Customer;
 }
