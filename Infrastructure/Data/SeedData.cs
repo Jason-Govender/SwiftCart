9fc66adbf8dcb2c8e743c8d56b9f18a0b22ac53f
Jason Govender <jasongovend@gmail.com>
2026-03-08 10:12:13 +0200

    Seeded Admin User

--- Infrastructure/Data/SeedData.cs
+++ Infrastructure/Data/SeedData.cs
@@ -1,5 +1,19 @@
+using SwiftCart.Domain.Entities;
+
 namespace SwiftCart.Infrastructure.Data;
 
 public static class SeedData
 {
+    public static void SeedUsersIfEmpty(AppDb db)
+    {
+        if (db.Users.Count > 0)
+            return;
+
+        db.Users.Add(new Administrator
+        {
+            Id = 1,
+            Username = "admin",
+            Password = "admin"
+        });
+    }
 }
