b58acc8bac404b3beec8ce43ccc991123ca88a13
Jason Govender <jasongovend@gmail.com>
2026-03-08 18:01:31 +0200

    Created Review Domain Entity

--- Domain/Entities/Review.cs
+++ Domain/Entities/Review.cs
@@ -2,4 +2,10 @@ namespace SwiftCart.Domain.Entities;
 
 public class Review
 {
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int ProductId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
 }
